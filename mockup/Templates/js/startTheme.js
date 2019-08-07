a4b1c9d8e94872e59439a203af35dc8d2e490573
TheCell <simon_hischier@hotmail.com>
2019-08-07 22:42:24 +0200

    started layout work

---
+++ mockup/Templates/js/startTheme.js
@@ -0,0 +1,23 @@
+function startTheme()
+{
+    // set obsidian as default theme
+    theme = new Theme(
+        {
+            background:"#22282a",
+            f_high:"#f1f2f3",
+            f_med:"#ec7600",
+            f_low:"#93c763",
+            f_inv:"#963a46",
+            b_high:"#678cb1",
+            b_med:"#4f6164",
+            b_low:"#42464C",
+            b_inv:"#ffcd22"
+        });
+    theme.install(document.body);
+    theme.start();
+}
+
+document.addEventListener('DOMContentLoaded', function(event)
+{
+    startTheme();
+});
