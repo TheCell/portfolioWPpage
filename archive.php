33448972f506f2dffd6253b027642e00c4d02238
TheCell <simon_hischier@hotmail.com>
2019-11-17 22:44:02 +0100

    removed sidebar

--- archive.php
+++ archive.php
@@ -9,18 +9,18 @@
 
 get_header();
 ?>
-
+<!--
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+-->
 		<?php if ( have_posts() ) : ?>
-
+			<!--
 			<header class="page-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			</header>--><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -36,7 +36,7 @@ get_header();
 
 			endwhile;
 
-			the_posts_navigation();
+			//the_posts_navigation();
 
 		else :
 
@@ -45,9 +45,9 @@ get_header();
 		endif;
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<!--</main>--><!-- #main -->
+	<!--</div>--><!-- #primary -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
